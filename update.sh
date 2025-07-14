33feb403d49b5bda776e01d609112f62a547bc44
prolletto64 <github@prolletto64.eu>
2025-07-14 20:19:49 +0200

    some update.sh tweaks

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ REPO_URL="ssh://aur@aur.archlinux.org/minecraft-technic-launcher.git"
 if [[ $1 ]];then
     UPDATED=$(echo $1 | sed s/\"//g)
     SHA256=$(curl "https://launcher.technicpack.net/launcher4/${UPDATED}/TechnicLauncher.jar" | sha256sum | sed s/"  -"//g)
-    git clone $REPO_RUL repo
+    git clone $REPO_URL repo
     cd repo
     cat ../PKGBUILD.template | sed s/CM_V/${UPDATED}/g | sed s/CM_S/${SHA256}/g > ./PKGBUILD
     makepkg --printsrcinfo > .SRCINFO
